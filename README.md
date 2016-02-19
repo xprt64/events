49a4c52a5bc56684b52e90ca1721eb77ae51c61e
Constantin GALBENU <xprt64@gmail.com>
2016-02-19 16:54:24 +0200

    moved the sse server from php to node

--- README.md
+++ README.md
@@ -7,7 +7,16 @@ to active web browser connections (like server sent events connections)
 
 # example
 see examples/ directory_
-open http://localhost/examples/index.html in a browser, in multiple tabs/windows_
-open a terminal on the localhost machine and run_
+
+open a terminal on the server and start the nodejs application: nodejs src/EventManager/app.js
+
+open http://yourserver/examples/index.html in a browser, in multiple tabs/windows_
+open a terminal on yourserver machine and run_
 `php -f examples/sse-backend-trigger.php`
 You should see the test events displayed in the browser_
+
+# note
+Initialy I implemented the sse frontent as a php script under apache but apache keeps creating
+process for every new sse request and don't close old ones; it should kill the script once the client closes the connection
+but is doesn't._
+So I implemented the sse server on nodejs, along with the event listener. 
--- src/EventManager/app.js
+++ src/EventManager/app.js
@@ -5,10 +5,9 @@
  * 
  * EventManager app
  */
-
-
 var dgram	= require("dgram");
-var net		= require("net");
+var http	= require("http");
+var url		= require('url');
 
 /**
  * The list with all registered event listeners
@@ -16,6 +15,8 @@ var net		= require("net");
  * @type object
  */
 var clients	=	{};
+var clientsLength	=	0;
+
 var i = 0;	//debug variable
 
 /**
@@ -49,16 +50,47 @@ receiver.on("error", function (err) {
 receiver.on("message", function (msg, rinfo) {
   console.log( (i++) + ": receiver got: " + msg.length + " bytes from " + rinfo.address + ":" + rinfo.port);
   
+  var json	=	JSON.parse(msg);
+   
+   if(clientsLength <= 0)
+	   console.log("no clients registered!");
+   
   //forward the event to all registered clients
   for(var j in clients)
   {
-	  var sock = clients[j];
+	  if(!clients.hasOwnProperty(j))
+		  continue;
 	  
-	   sock.write(msg);
+	  var client = clients[j];
+	  
+	  if(!json.userId || client.userId == json.userId)
+	  {
+		 client.response.write('event: ' + (json.type || 'ping') + "\n");
+		 if(json.id)
+			 client.response.write('id: ' + json.id + "\n");
+		 client.response.write('data: ' + msg + "\n\n");
 	   
-	    console.log("forwarded to client: " + j);
-  }
+	    console.log("forwarded to client #" + j);
+	}
+	else//we don't forward event that are for other user
+	    console.log("not forwarded to client: " + j);
+ }
 });
+
+setInterval(function() {
+	for (var j in clients)
+	{
+		if (!clients.hasOwnProperty(j))
+			continue;
+		
+		var client = clients[j];
+
+		client.response.write("event: ping\n");
+		client.response.write('data: ' + 'stai alive' + "\n\n");
+
+		console.log("pinged client #" + j);
+	}
+}, 60*10 * 1000);
 
 receiver.on("listening", function () {
   var address = receiver.address();
@@ -71,26 +103,51 @@ receiver.bind(UDP_PORT, '127.0.0.1');
 /**************************
  * The event listeners manager
  */
-listenersManager = net.createServer(function(sock){
-	console.log("new client registered:" + sock.remoteAddress + ':' + sock.remotePort);
+listenersManager = http.createServer();
+
+listenersManager.on('request', function(req, response){
+
+	var sock		=	req.connection;
+	var client_id	=	sock.remoteAddress + ':' + sock.remotePort + '#' + Math.random();
+	var url_parts	=	url.parse(req.url, true);
 	
-	var client_id	=	sock.remoteAddress + ':' + sock.remotePort;
+	console.log("new client request registration:" + sock.remoteAddress + ':' + sock.remotePort);
 	
-	clients[client_id]	=	sock;
+	// add client to clients list
+	clients[client_id]	=	{
+		response:	response,
+		userId:		url_parts.query.userId
+	};
+
+	clientsLength++;
+
+	console.log("clientsLength:" + clientsLength);
+	
+	
+	response.writeHead(200, {
+		'Content-Type'					:	'text/event-stream', 
+		'Access-Control-Allow-Origin'	:	'*'
+	});
 	
 	sock.on('close', function(){
 		console.log("client closed:" + client_id);
-		
-		delete clients[client_id];
 	});
 	
 	sock.on('end', function(){
 		console.log("client ended:" + client_id);
 		
 		delete clients[client_id];
+		
+		clientsLength--;
 	});
 	
 	sock.on('error', function(){
 		console.log("error event:" + client_id);
 	});
 }).listen(TCP_PORT, '0.0.0.0');
+
+
+function pecho(s)
+{
+	console.log(s);
+}
